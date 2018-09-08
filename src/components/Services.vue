11de1e8b5c4065226643ad31bbccd5ffe361a7c9
Bryan Wegener <bryan.wegener@microchip.com>
2018-09-08 15:03:32 -0700

    services - minor css changes

--- src/components/Services.vue
+++ src/components/Services.vue
@@ -3,50 +3,62 @@
 		<div class="service-container">
 			<div class="card">
 				<div class="card-image waves-effect waves-block waves-light">
-				<img class="activator" src="../assets/logo.png">
+					<img class="activator" src="../assets/logo.png">
 				</div>
 				<div class="card-content">
-				<span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
+					<span class="card-title activator grey-text text-darken-4">Exterior Painting</span>
 				</div>
 				<div class="card-reveal">
-				<span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">X</i></span>
-				<p>Here is some more information about this product that is only revealed once clicked on.</p>
+					<span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">X</i></span>
+					<p>Here is some more information about this product that is only revealed once clicked on.</p>
 				</div>
 			</div>
 			<div class="card">
 				<div class="card-image waves-effect waves-block waves-light">
-				<img class="activator" src="../assets/logo.png">
+					<img class="activator" src="../assets/logo.png">
 				</div>
 				<div class="card-content">
-				<span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
+					<span class="card-title activator grey-text text-darken-4">Interior Painting</span>
 				</div>
 				<div class="card-reveal">
-				<span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">X</i></span>
-				<p>Here is some more information about this product that is only revealed once clicked on.</p>
+					<span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">X</i></span>
+					<p>Here is some more information about this product that is only revealed once clicked on.</p>
 				</div>
 			</div>
 			<div class="card">
 				<div class="card-image waves-effect waves-block waves-light">
-				<img class="activator" src="../assets/logo.png">
+					<img class="activator" src="../assets/logo.png">
 				</div>
 				<div class="card-content">
-				<span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
+					<span class="card-title activator grey-text text-darken-4">Kool Decking</span>
 				</div>
 				<div class="card-reveal">
-				<span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">X</i></span>
-				<p>Here is some more information about this product that is only revealed once clicked on.</p>
+					<span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">X</i></span>
+					<p>Here is some more information about this product that is only revealed once clicked on.</p>
 				</div>
 			</div>
 			<div class="card">
 				<div class="card-image waves-effect waves-block waves-light">
-				<img class="activator" src="../assets/logo.png">
+					<img class="activator" src="../assets/logo.png">
 				</div>
 				<div class="card-content">
-				<span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
+					<span class="card-title activator grey-text text-darken-4">New Stucco</span>
 				</div>
 				<div class="card-reveal">
-				<span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">X</i></span>
-				<p>Here is some more information about this product that is only revealed once clicked on.</p>
+					<span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">X</i></span>
+					<p>Here is some more information about this product that is only revealed once clicked on.</p>
+				</div>
+			</div>
+			<div class="card">
+				<div class="card-image waves-effect waves-block waves-light">
+					<img class="activator" src="../assets/logo.png">
+				</div>
+				<div class="card-content">
+					<span class="card-title activator grey-text text-darken-4">New Stucco</span>
+				</div>
+				<div class="card-reveal">
+					<span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">X</i></span>
+					<p>Here is some more information about this product that is only revealed once clicked on.</p>
 				</div>
 			</div>
 		</div>
@@ -67,6 +79,10 @@ export default {};
   .card {
     padding: 0 10px;
     margin: 10px;
+
+    &:hover {
+      background-color: orange;
+    }
   }
 }
 </style>
